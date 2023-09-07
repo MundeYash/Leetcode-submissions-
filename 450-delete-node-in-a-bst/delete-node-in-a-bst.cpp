9c4952b75564d58809ee85f7d88320ce62352d41
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2023-09-08 01:39:34 +0530

    Time: 19 ms (97.49%) | Memory: 32.7 MB (18.30%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -11,51 +11,76 @@
  */
 class Solution {
 public:
-        TreeNode* findMin(TreeNode* node) {
-            while (node->left) {
-                node = node->left;
-            }
-            return node;
-        }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == NULL ) {
-        return root; // Tree is empty or value not found
+
+
+// function for deleting a particular / node->data value in the binary search tree 
+TreeNode* minValue (TreeNode* root ){
+    if (!root )return NULL ;
+
+    while (root->left){
+        root= root->left ;
     }
+    return root ;
 
-    if (key < root->val) {
-
-        // move to the left 
-        root->left = deleteNode(root->left, key);
-    } else if (key > root->val) {
-        // move to right 
-        root->right = deleteNode(root->right, key);
-    } else {
-        // Node with the key value found, delete it
+}
 
 
-        // Case 1: Node with no child
-        if (root ->left == NULL and root ->right == NULL ){
+
+    TreeNode* deleteNode(TreeNode* root, int val) {
+        // delete the node whose data is equal to the val 
+
+    if (!root )return root  ; 
+
+    // recursive case 
+    if (root ->val == val ){
+
+        // important case , 4 options are there for the deletion 
+
+        // 0 child exits 
+        if(root->left == NULL and root ->right == NULL ){
             delete root ; 
             return NULL ;
         }
 
-        // Case 1: Node with only one child or no child
-        if (root->left == nullptr) {
-            TreeNode* temp = root->right;
-            delete root;
-            return temp;
-        } else if (root->right == nullptr) {
-            TreeNode* temp = root->left;
-            delete root;
-            return temp;
+        // 1child exists (either left or right child )
+
+        if (root->left == NULL and root -> right != NULL ){
+            // store the right node 
+            TreeNode* temp = root->right  ;
+            delete root ; 
+            return temp ;
         }
 
-        // Case 2: Node with two children
-        TreeNode* temp = findMin(root->right); // Find the inorder successor
-        root->val = temp->val; // Copy the successor's data to this node
-        root->right = deleteNode(root->right, temp->val); // Delete the successor
+        if (root->left != NULL and root->right == NULL ){
+            TreeNode* temp = root->left ; 
+            delete root ; 
+            return temp ;
+        }
+
+        // 2child exits (both left and right ){either find maximum from left subtree and replace it with root  or find minimum element form the right and replace it with root , do recursvive call}
+
+        if (root ->left != NULL and root ->right != NULL ) {
+            int mini = minValue (root ->right )->val ;// gives minimum value from the right 
+            root ->val = mini ; // add min value to the root value 
+            root ->right = deleteNode(root->right , mini);
+            return root ;
+
+        }
+
     }
 
-    return root;
+    else if (root ->val <val ){
+        // val exists in right subtree (move to the right )
+        root ->right = deleteNode(root->right , val) ; 
+        return root ;
+
+    }
+    else{
+        // val exists in the left subtree (move to the left )
+        root ->left = deleteNode(root->left , val);
+        return root ;
+    }
+
+    return root ;
     }
 };
