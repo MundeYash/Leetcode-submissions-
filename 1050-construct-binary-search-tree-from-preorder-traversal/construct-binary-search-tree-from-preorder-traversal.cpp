94fc3cf3f10cbf88fa84c5be8b3bb2d88406d41f
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2023-09-13 12:07:34 +0530

    Time: 7 ms (28.55%) | Memory: 21.9 MB (6.11%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,29 +12,46 @@
 class Solution {
 public:
 
-    TreeNode* solve(vector<int> pre , int mini, int maxi , int &index ){
-        // base case 
-        if (index >= pre.size()) return NULL ;
-
-        if (pre[index]<mini || pre[index]>maxi )return NULL ; 
-
-        // create root 
-        TreeNode* root = new TreeNode(pre[index++]) ; 
-        root ->left = solve(pre , mini , root->val , index ) ; 
-        root ->right = solve(pre , root->val , maxi , index  ) ; 
-        
-
-        return root ;
+int search(vector<int> in, int element)
+{
+    for (int i = 0; i < in.size(); i++)
+    {
+        if (in[i] == element)
+            return i;
     }
+    return -1;
+}
+TreeNode *solve2(vector<int> pre, vector<int> in, int n, int &index, int inorderStart, int inorderEnd)
+{
+    if (inorderStart > inorderEnd || index >= n)
+        return NULL;
+
+    int element = pre[index++];
+
+    TreeNode *root = new TreeNode(element);
+    int position = search(in, element);
+    root->left = solve2(pre, in, n, index, inorderStart, position - 1);
+    root->right = solve2(pre, in, n, index, position + 1, inorderEnd);
+
+    return root;
+}
 
     TreeNode* bstFromPreorder(vector<int>& pre) {
-        
-        int n = pre.size() ; 
-        int index =0 ;
-        return solve(pre , INT_MIN , INT_MAX ,  index ) ;
+         // create tree from the inorder(get by sorting the preorder ) and pre order
 
+    // time : O(n logn )  , space : O(1);
 
+    // step 1 : find the inorder traversal by sorting preorder
+    vector<int> in = pre;
+    sort(in.begin(), in.end());
 
+    // step 2 : appy the concept of construct tree form inorder and pre order
+    int preorderIndex = 0;
+    int n = in.size();
+    int inorderStart = 0, inorderEnd = n - 1;
+    int index = 0;
 
+    TreeNode *root = solve2(pre, in, n, index, inorderStart, inorderEnd);
+    return root;
     }
 };
