7033d2b34fc2030d62ab3fd04bf2669da2b70098
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-02-12 21:12:23 +0530

    Time: 14 ms (27.67%) | Memory: 21.7 MB (13.57%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,25 +12,50 @@
 class Solution {
 public:
 
-    // Brute Force Approach : O(n^2) :because of finding the height for each and then checking its balanced or not 
-    int height (TreeNode* root ){
-        if (!root )return 0 ; 
-        return max ( height(root->left ), height(root->right) ) + 1 ;
+    // // Brute Force Approach : O(n^2) :because of finding the height for each and then checking its balanced or not 
+    // int height (TreeNode* root ){
+    //     if (!root )return 0 ; 
+    //     return max ( height(root->left ), height(root->right) ) + 1 ;
+    // }
+
+
+    // OPTIMISED APPROACH : Time : O(n) ,space: O(n)-> recursive Stack Space 
+    pair<bool , int> fastIsBalanced(TreeNode* root ){
+         pair<bool , int> ans ; 
+         if (!root ){
+             ans.first = true ; 
+             ans.second = 0 ; 
+             return ans;
+         }
+
+          pair<bool , int> leftAns = fastIsBalanced (root->left ) ; 
+           pair<bool , int> rightAns = fastIsBalanced(root ->right ) ; 
+
+          if (leftAns.first and rightAns.first and abs(leftAns.second - rightAns.second)<=1 ){
+              ans.first  = true ; 
+          } 
+          else{
+              ans.first  = false;
+          }
+          ans.second = max(leftAns.second , rightAns.second )  + 1; 
+          return ans;
     }
     bool isBalanced(TreeNode* root) {
         // base case 
         if (!root )return true ; 
-        // work 
-        bool leftAns  = isBalanced(root ->left ) ; 
-        bool rightAns = isBalanced(root ->right ); 
-        int leftHeight = height(root ->left ) ; 
-        int rightHeight = height (root ->right ) ; 
-        if (leftAns and rightAns and abs(leftHeight - rightHeight )<=1 ){
-            return true ; 
-        }
-        else{
-            return false;
-        }
+        // // work 
+        // bool leftAns  = isBalanced(root ->left ) ; 
+        // bool rightAns = isBalanced(root ->right ); 
+        // int leftHeight = height(root ->left ) ; 
+        // int rightHeight = height (root ->right ) ; 
+        // if (leftAns and rightAns and abs(leftHeight - rightHeight )<=1 ){
+        //     return true ; 
+        // }
+        // else{
+        //     return false;
+        // }
+
+         return fastIsBalanced(root ).first ; 
 
         
     }
