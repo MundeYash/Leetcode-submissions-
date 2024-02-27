f4584e4235234b11e917091c2a4f9cf2934fe87a
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-02-27 23:05:20 +0530

    Time: 3 ms (95.23%) | Memory: 19 MB (46.45%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,28 +11,30 @@
  */
 class Solution {
 public:
-    pair<int , int > fastDia (TreeNode* root ){
-        pair<int , int> ans; 
+    pair<int ,int > solve(TreeNode* root ){
+        // base case 
+        pair<int ,int> ans; 
         if (!root ){
-            ans.first = 0  ; 
-            ans.second =  0 ; 
+            ans.first = 0 ; 
+            ans.second =0 ;
             return ans;
-
         }
 
-        //recursive 
-        pair<int , int > leftAns = fastDia(root->left ); 
-        pair<int , int > rightAns  = fastDia(root ->right );
+        pair<int ,int >  leftAns = solve(root ->left ) ;
+        pair<int ,int > rightAns = solve(root ->right ); 
+
         int op1 = leftAns.first ; 
         int op2 = rightAns.first ; 
-        int op3 = (leftAns.second + rightAns.second ) ; 
-        ans.first = max(op1 , max(op2 , op3 )) ; 
-        ans.second = max(leftAns.second , rightAns.second ) + 1 ; 
+        int op3 = (leftAns.second + rightAns.second )+ 1; 
+        ans.first = max(op1, max(op2,op3 )); 
+        ans.second  =max(leftAns.second , rightAns.second ) + 1; 
+
         return ans;
+        
+
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        //Optimsed Approach : finding the diameter in the optimsed way int O(n) time complexity 
-        return fastDia(root ).first ; 
-
+        if (!root )return 0 ; 
+        return solve(root ).first -1 ; // because number of edges are one smaller than actual no of nodes 
     }
 };
