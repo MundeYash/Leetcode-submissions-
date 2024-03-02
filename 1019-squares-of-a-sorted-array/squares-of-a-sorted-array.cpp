01e3530d0c2c5c6c6804f0932f3825e172d7b9d5
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-03-02 16:13:19 +0530

    Time: 25 ms (53.25%) | Memory: 28.4 MB (46.74%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -3,10 +3,23 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         // sort the elements without the sign //0 1 3 4 10
         int n = nums.size(); 
-        for (int i=0 ;i<n ;i++ ){
-            nums[i] = (nums[i]* nums[i]);
+    // optimised approach : Two pointer approach , time : O(N) , space : O(n-> due to using of extra array )
+    int start= 0 ,end =n-1 ; 
+    // since the array is sorted in increasing (just consider the absolute value means only maginitue )
+    vector<int> ans (n,0);
+    int index =n-1 ;
+    while(start<= end and index>=0 ){
+        if (abs(nums[start])>= abs(nums[end])){
+            // means negative value is greater 
+            ans[index --] = (nums[start]* nums[start ]); 
+            start ++ ; 
+        
         }
-        sort(nums.begin() , nums.end()); 
-        return nums ; 
+        else{
+            ans[index -- ] = (nums[end]* nums[end ]) ; 
+            end -- ; 
+        }
+    }
+        return ans ; 
     }
 };
