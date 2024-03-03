2855982e83767eb8290d8220590439a15c1883b4
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-03-03 23:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (45.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,47 +10,50 @@
  */
 class Solution {
 public:
-int lengthLL(ListNode* head ){
-    int count =0 ; 
-    while (head != NULL){
-        count++ ;
-        head = head->next ;
-    }
-    return count ;
-}
-    ListNode* removeNthFromEnd(ListNode* head, int k) {
-        // time : O(2n) ,space : O(1)
-        if (head == NULL) return NULL ; 
-        int n = lengthLL(head) ;
-
-        if (n == k ){
-            // deletion at front 
-            ListNode*temp  =  head; 
-            head = head ->next ; 
-            temp->next = NULL  ;
-            delete temp ; 
-            return head ;
-
-        }
-
-        k = k % n ; 
-
-        int count =1 ; 
-        ListNode* temp = head ;
-        while(count <n-k ){
+    int  lengthLL(ListNode* head ){
+        int count =0 ; 
+        while (head ){
             count ++ ; 
-            temp = temp->next ;
+            head = head ->next; 
+        }
+        return count ; 
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int k) {
+        if (!head|| (!head->next and k==1 ) )return NULL ; // no need of deletion 
+
+        // step 1: find the length of the link list 
+        int n = lengthLL (head ); //O(n)
+        ListNode* temp = head;
+        if (k== n ){
+            // deletion of head node 
+            head = head ->next; 
+            temp->next = NULL ; 
+            delete temp ; 
+            return head; 
+        }
+
+        // other cases 
+        k = k %n ; // to make the k in the range of n 
+        int cnt =1 ;
+
+        while(cnt<(n-k)){//O(k)
+
+            temp = temp->next; 
+            cnt ++ ; 
+        }
+
+        // we have to delte the temp ->next node 
+        if (temp->next == NULL || k==0 ){
+            // deletion of last node which is null and (no need of deletion )
+            return head ; 
 
         }
-        if (temp->next == NULL){
-            return head;
-        }
-        ListNode* newNode = temp->next ; 
-        temp->next = newNode->next; 
-        newNode->next = NULL ; 
-        delete newNode ; 
-        return head;
 
-        
+        // other solvable case 
+        ListNode* delNode = temp->next; 
+        temp->next = delNode->next; 
+        delete delNode; 
+        return head; 
+
     }
 };
