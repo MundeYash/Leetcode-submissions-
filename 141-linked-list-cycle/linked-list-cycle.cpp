0be3c22959d599064292e0fd98c13be40be5b919
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-03-06 10:13:19 +0530

    Time: 15 ms (14.99%) | Memory: 14.7 MB (5.66%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,23 +8,16 @@
  */
 class Solution {
 public:
-
-// two pointer concept  , time complexity O(n) , space :O(n)
     bool hasCycle(ListNode *head) {
-      // brute force approach : hastset appraoch  ;
-      unordered_set<ListNode* > st ; 
-      while (head != NULL){
+        //Concept : -> map based solution : 
 
-          // check the prsence ;
-          if(st.find(head)!= st.end()){
-              return true  ; // presence of node aready means loop 
-          }
-          // insert head 
-          st.insert(head) ; 
-          // update the head ; 
-          head = head->next;
-      }
-      return false ;
+        unordered_map<ListNode* ,int > mp ; 
+        while (head ){
+            if (mp.find(head)!= mp.end())return true ;
+            mp[head] = head->val ; 
+            head = head-> next ;
+        }
+        return false;
         
     }
 };
