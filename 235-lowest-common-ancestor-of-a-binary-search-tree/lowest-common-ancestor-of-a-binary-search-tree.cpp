9f96781d582012f53d16e990480cbf80610693c6
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-04-16 04:39:13 +0530

    Time: 30 ms (8.64%) | Memory: 21.7 MB (64.52%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,32 +11,32 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // simple approch:Post Order traversal & recursion based 
 
-         // base case 
-        if (!root )return NULL ; 
-
-        // recursive case 
-        if (root == p || root ==  q ){
-            return root ; 
+        // base case 
+        if (!root ){
+            return NULL ; 
         }
 
-        TreeNode* leftAns = lowestCommonAncestor(root->left , p , q ) ; 
-        TreeNode* rightAns = lowestCommonAncestor (root->right , p ,q ) ; 
+        if (root->val == p->val || root->val == q->val  ){
+            return root  ;
+        }
 
-        if (leftAns!= NULL  and rightAns != NULL ){
-            // means both finded the answer 
-            return root ; 
-        }
-        else if (leftAns!= NULL  and rightAns == NULL ){
-            return leftAns ;
+        TreeNode* leftAns = lowestCommonAncestor(root->left , p, q ); 
+        TreeNode* rightAns = lowestCommonAncestor(root->right , p , q );
 
+        // work 
+        if (leftAns == NULL and rightAns == NULL ){
+            return NULL ; 
         }
-        else if (leftAns== NULL and rightAns != NULL ){
-            return rightAns ;
+      
+        else if (leftAns != NULL and rightAns!=NULL ){
+            return root ;
         }
-        else {
-            return NULL ; // no common ancestor exits 
+          else if (left!=NULL || rightAns !=NULL ){
+            if (leftAns!=NULL )return leftAns ; 
+            else return rightAns;
         }
-        
+
     }
 };
