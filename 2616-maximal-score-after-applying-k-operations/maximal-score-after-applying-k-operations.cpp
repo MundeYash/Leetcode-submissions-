a7cf09bef47307d22785d500ab4e658fabf37922
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-10-14 23:20:28 +0530

    Time: 472 ms (5.24%) | Memory: 86.1 MB (9.23%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-long long manualCeil(double val) {
-    // Get the integer part of the value by casting to long long
-    long long intPart = static_cast<long long>(val);
 
-    // If val is positive and has a fractional part, increment the integer part
-    if (val > intPart) {
-        return intPart + 1;
-    }
-    // Otherwise, return the integer part as it is
-    return intPart;
-}
     long long maxKelements(vector<int>& nums, int k) {
         // priority queue , max heap based question 
         priority_queue<long long > pq ; 
