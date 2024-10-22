a133fd8a1cb80ef14360bd12af9b456873030d31
Y@$HMuNdE : ) <109969817+MundeYash@users.noreply.github.com>
2024-10-22 21:11:49 +0530

    Time: 13 ms (99.32%) | Memory: 156.9 MB (5.08%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -9,53 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ bool comp(long long a, long long b ){
+    if (a>=b)return true ;
+    return false;
+ }
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        // concept :Level Order Traversal 
-
-        queue<TreeNode* > q; 
+        // level sum store and sorting for kth largest value 
+        long long ans =0 ; 
+        queue<TreeNode*> q; 
         q.push(root ) ; 
-
-        vector<long long > ans;
-
-       
-
+        vector<long long >temp ; 
         while (!q.empty()){
-            int level = q.size() ; 
-            long long  sum =0 ;
-
-            for (int i=0 ;i< level ;i++)
-            {
-
-                TreeNode* frontNode = q.front () ; 
-                q.pop() ; 
-
-                sum +=(long long ) frontNode->val ; 
+            int n = q.size() ; 
+            long long sum =0 ; 
+            for (int i=0 ;i<n ;i++ ){
+                TreeNode* frontNode = q.front() ; 
+                q.pop();
+                sum += frontNode->val ; 
 
                 if (frontNode->left ){
-                    q.push(frontNode->left ) ;
+                    q.push(frontNode->left );
                 }
+
                 if (frontNode->right ){
-                    q.push(frontNode->right );
+                    q.push(frontNode->right);
                 }
-
             }
+            // this means completion of one level 
+            temp.push_back(sum );
+        }
+        sort(temp.begin(),temp.end(),comp);
   
-            ans.push_back(sum);
-        }
-
- 
-
-
-        // sort the ans and than the kth largest sum 
-        int n = ans.size();
-        sort(ans.begin()  ,ans.end());
-
-        if (k>n ){
-            return -1 ;
-        }
-        return ans[n-k];
-        
+        if (temp.size()<k)return -1 ; 
+        return temp[k-1];
     }
 };
